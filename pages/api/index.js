d4a55fbf6d6ecb778e60f1392b9e75c412f09ace
realmojo88 <strikers1999@naver.com>
2022-05-23 17:50:21 +0900

    console..og rempvoed

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,5 +1,5 @@
 import dbConnect from "../../lib/mongodb";
-console.log(`process.env.NODE_ENV: ${process.env.NODE_ENV}`);
+
 const handler = async (_, res) => {
   res.status(500).json({ message: "hello" });
 };
